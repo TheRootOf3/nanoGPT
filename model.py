9cf7e1f96e40ac46836c51df3288a79360d6b7de
Andrzej Szablewski <as3623@cam.ac.uk>
2025-06-21 00:19:06 +0100

    Made the computation of model FLOPs more specific to the number of attention heads in each layer.
    Signed-off-by: Andrzej Szablewski <as3623@cam.ac.uk>

--- model.py
+++ model.py
@@ -647,8 +647,11 @@ class GPT(nn.Module):
         # see PaLM paper Appendix B as ref: https://arxiv.org/abs/2204.02311
         N = self.get_num_params()
         cfg = self.config
-        L, H, Q, T = cfg.n_layer, cfg.n_head, cfg.n_embd // cfg.n_head, cfg.block_size
-        flops_per_token = 6 * N + 12 * L * H * Q * T
+        H, Q, T = cfg.n_head, cfg.n_embd // cfg.n_head, cfg.block_size
+        flops_per_token = 6 * N
+        for block in self.transformer.h:
+            H = len(block.attn.trainable_heads)
+            flops_per_token += 12 * H * Q * T
         flops_per_fwdbwd = flops_per_token * T
         flops_per_iter = flops_per_fwdbwd * fwdbwd_per_iter
         # express our flops throughput as ratio of A100 bfloat16 peak flops
